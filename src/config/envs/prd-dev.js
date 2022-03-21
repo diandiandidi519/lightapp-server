caca97d7400b035a97badcda6416124d31a05c8a
wangshuya <wangshuya.wsy@bytedance.com>
2022-03-22 00:13:30 +0800

    feat: 线上数据库地址

--- src/config/envs/prd-dev.js
+++ src/config/envs/prd-dev.js
@@ -3,20 +3,20 @@
  */
 const devConf = require('./dev')
 
-// // 修改redis连接配置
-// Object.assign(devConf.redisConf, {
-//     // 和 docker-compose 中配置的service名字一致
-//     host: 'editor-redis',
-// })
-// // 修改mysql连接配置
-// Object.assign(devConf.mysqlConf, {
-//     // 和 docker-compose 中配置的service名字一致
-//     host: 'editor-mysql',
-// })
-// // 修改mongo连接配置
-// Object.assign(devConf.mongodbConf, {
-//     // 和 docker-compose 中配置的service名字一致
-//     host: 'editor-mongo',
-// })
+// 修改redis连接配置
+Object.assign(devConf.redisConf, {
+    // 和 docker-compose 中配置的service名字一致
+    host: '59.110.140.36',
+})
+// 修改mysql连接配置
+Object.assign(devConf.mysqlConf, {
+    // 和 docker-compose 中配置的service名字一致
+    host: '59.110.140.36',
+})
+// 修改mongo连接配置
+Object.assign(devConf.mongodbConf, {
+    // 和 docker-compose 中配置的service名字一致
+    host: '59.110.140.36',
+})
 
 module.exports = devConf
