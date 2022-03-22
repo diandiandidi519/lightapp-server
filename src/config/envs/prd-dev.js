0acfc7a62215127a4b90e832bc00bd85f7f996cf
wangshuya <wangshuya.wsy@bytedance.com>
2022-03-22 21:06:58 +0800

    feat: deploy

--- src/config/envs/prd-dev.js
+++ src/config/envs/prd-dev.js
@@ -5,18 +5,16 @@ const devConf = require('./dev')
 
 // 修改redis连接配置
 Object.assign(devConf.redisConf, {
-    // 和 docker-compose 中配置的service名字一致
-    host: '59.110.140.36',
+    // 如果和docker实例一起编排 和docker-compose 中配置的service名字一致
+    host: 'lightapp-redis',
 })
 // 修改mysql连接配置
 Object.assign(devConf.mysqlConf, {
-    // 和 docker-compose 中配置的service名字一致
-    host: '59.110.140.36',
+    host: 'lightapp-mysql',
 })
 // 修改mongo连接配置
 Object.assign(devConf.mongodbConf, {
-    // 和 docker-compose 中配置的service名字一致
-    host: '59.110.140.36',
+    host: 'lightapp-mongo',
 })
 
 module.exports = devConf
--- src/config/envs/prd.js
+++ src/config/envs/prd.js
@@ -3,5 +3,17 @@
  */
 const devConf = require('./dev')
 
-
+// 修改redis连接配置
+Object.assign(devConf.redisConf, {
+    // 通过修改hosts文件指向本机IP地址
+    host: 'docker-host',
+})
+// 修改mysql连接配置
+Object.assign(devConf.mysqlConf, {
+    host: 'docker-host',
+})
+// 修改mongo连接配置
+Object.assign(devConf.mongodbConf, {
+    host: 'docker-host',
+})
 module.exports = devConf
